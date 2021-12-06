6c6d8637e9237aa0cf68bb025dfcc0c8a976a7f8
Yrd-Q <94402417+Yrd-Q@users.noreply.github.com>
2021-12-07 00:53:34 +0200

    Добавено Review (базов клас)

---
+++ src/SchoolbookApp/SchoolbookApp/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolbookApp.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string StudentId { get; set; }
+        public SchoolClass SchoolClass { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public Review()
+        {
+
+        }
+
+    }
+}
