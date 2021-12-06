49d0de2ea36d0f57ad30587ba3d6225ba0b05bb4
Yrd-Q <94402417+Yrd-Q@users.noreply.github.com>
2021-12-06 17:50:50 +0200

    Начално качване
    #4

---
+++ src/SchoolbookApp/SchoolbookApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SchoolbookApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
