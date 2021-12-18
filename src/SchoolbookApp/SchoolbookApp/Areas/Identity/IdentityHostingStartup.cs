632a7c4bf9012d4220b02645eeadbaf3f034a08e
Imortalco <vanko.krastev@gmail.com>
2021-12-18 15:56:07 +0200

    Редактиране на register страницата и добавяне на роли
    Създаване на метод за добавяне на роли в Startup. cs , за да можете да си ги добавите в базата данни.В зависимост от ролатя на логнатия акаун, приложението те пренасочва към съответната Main страница. Регистрацията вече става възможна само от акаунт с роля "Admin".Ако нямате такъв акаунт, махнете бутона от if блока на Home/Index ,и след като си направите пак го върнете там :D.

---
+++ src/SchoolbookApp/SchoolbookApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SchoolbookApp.Data;
+
+[assembly: HostingStartup(typeof(SchoolbookApp.Areas.Identity.IdentityHostingStartup))]
+namespace SchoolbookApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- src/SchoolbookApp/SchoolbookApp/Startup.cs
+++ src/SchoolbookApp/SchoolbookApp/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SchoolbookApp.Data;
+using SchoolbookApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,15 +31,29 @@ namespace SchoolbookApp
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDatabaseDeveloperPageExceptionFilter();
-
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.Configure<IdentityOptions>(options => {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredUniqueChars = 2;
+                
+                options.User.RequireUniqueEmail = true;
+                
+
+            });
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddRazorPages();
             services.AddControllersWithViews();
+            services.AddScoped<UserManager<ApplicationUser>>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -58,7 +73,7 @@ namespace SchoolbookApp
 
             app.UseAuthentication();
             app.UseAuthorization();
-
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
@@ -66,6 +81,32 @@ namespace SchoolbookApp
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+            //Разкодирайте тук и пуснете приложението.Като ги добавите ролите го закодирайте пак :)
+            //CreateUserRoles(serviceProvider).Wait();
+            
+        }
+        //Метод за добавяне на роли в базата данни
+        private async Task CreateUserRoles(IServiceProvider serviceProvider)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            var roles = new List<string>() 
+            { 
+                "Admin","Student","Parent","Teacher"
+            };
+            IdentityResult roleResult;
+            
+            foreach (var role in roles)
+            {
+                var roleCheck = await RoleManager.RoleExistsAsync(role);
+                if (!roleCheck)
+                {
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+            
+            //ApplicationUser user = await UserManager.FindByEmailAsync("admin@admin.com");
+            //await UserManager.AddToRoleAsync(user, "Admin");
         }
     }
 }
